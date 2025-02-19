8f5cfe0d33eb1cd31d3d5e61ece2d1029772f269
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2025-02-19 23:49:15 +0600

    update-3

--- client/src/layouts/Secctions/section1.jsx
+++ client/src/layouts/Secctions/section1.jsx
@@ -1,30 +1,56 @@
-import Card1 from "../../components/card1";
-import Card2 from "../../components/card2";
-import Card3 from "../../components/card3";
-import Card4 from "../../components/card4";
+import { useState, useEffect } from "react";
+import "../../Styles/section2_style/sec1.css";
+import img1 from "../../assets/cart.png";
+import img2 from "../../assets/amex.png";
+import img3 from "../../assets/About us.png";
 
+const images = [
+  { url: img1, text: "Welcome to Image 1" },
+  { url: img2, text: "Discover Image 2" },
+  { url: img3, text: "Explore Image 3" },
+];
 
-import "../../Styles/section2_style/Card1.css"
 const Section1 = () => {
-  return (
-    <div className="section1_container">
-      <div>
-      <Card1/>
-      </div>
-      <div>
-      <Card2/>
-      </div>
-      
-      <div>
-      <Card3/>
-     
-      </div>
-      <div>
-      <Card4/>
-      </div>
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showText, setShowText] = useState(false);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowText(false);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setTimeout(() => {
+        setShowText(true);
+      }, 2000);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const nextSlide = () => {
+    setShowText(false);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setTimeout(() => {
+      setShowText(true);
+    }, 2000);
+  };
+
+  const prevSlide = () => {
+    setShowText(false);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setTimeout(() => {
+      setShowText(true);
+    }, 1000);
+  };
+
+  return (
+    <div className="slider-container">
+      <button className="prev" onClick={prevSlide}>&#10094;</button>
+      <div className="slider" style={{ backgroundImage: `url(${images[currentIndex].url})` }}>
+        {showText && <div className="text-overlay">{images[currentIndex].text}</div>}
+      </div>
+      <button className="next" onClick={nextSlide}>&#10095;</button>
     </div>
-  )
-}
+  );
+};
 
 export default Section1;
--- client/src/layouts/Secctions/section2.jsx
+++ client/src/layouts/Secctions/section2.jsx
@@ -1,8 +1,30 @@
-
+import Card1 from "../../components/card1";
+import Card2 from "../../components/card2";
+import Card3 from "../../components/card3";
+import Card4 from "../../components/card4";
 
 const Section2 = () => {
   return (
-    <div>section2</div>
+    <div className="section1_container">
+       
+           
+        
+      <div>
+      <Card1/>
+      </div>
+      <div>
+      <Card2/>
+      </div>
+            
+      <div>
+      <Card3/>
+     
+      </div>
+      <div>
+      <Card4/>
+      </div>
+
+    </div>
   )
 }
 
