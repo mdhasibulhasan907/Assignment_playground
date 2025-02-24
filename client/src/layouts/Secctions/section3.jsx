35ff35914fc73944360535e9ee789c4870621c57
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2025-02-24 20:20:01 +0600

    section7 update

--- client/src/layouts/Secctions/section3.jsx
+++ client/src/layouts/Secctions/section3.jsx
@@ -1,25 +1,21 @@
 import S_card1 from "../../components/sec3_cards/s_card1";
-import "../../Styles/section2_style/sec3.css"
+import "../../Styles/section2_style/sec3.css";
 
 const Section3 = () => {
   return (
     <div className="cards-container">
-    <S_card1 date={7} month="May" title="Fun colourful cutlery for toddlers to more enjoy dining"
+    <S_card1 date={5} month="MAY" title="Fun colourful cutlery for toddlers to more enjoy dining"
         des="Pretium fusce id velit ut tortor. Euismod quis viverra nibh
             cras pulvinar mattis nunc. Arcu felis bibendum ut tristique et egestas
             ipsum. Sed risus ultricies tristique nulla aliquet."
         more="READ MORE"
     />
-    <S_card1 date={7} month="May" title="Fun colourful cutlery for toddlers to more enjoy dining"
-        des="Pretium fusce id velit ut tortor. Euismod quis viverra nibh
-            cras pulvinar mattis nunc. Arcu felis bibendum ut tristique et egestas
-            ipsum. Sed risus ultricies tristique nulla aliquet."
+    <S_card1 date={9} month="MAY" title="Fun childhood with furry toys ready for best cuddles "
+        des="Pretium fusce id velit ut tortor. Euismod quis viverra nibh cras pulvinar mattis nunc. Arcu felis bibendum ut tristique et egestas ipsum. Sed risus ultricies tristique nulla aliquet. "
         more="READ MORE"
     />
-    <S_card1 date={7} month="May" title="Fun colourful cutlery for toddlers to more enjoy dining"
-        des="Pretium fusce id velit ut tortor. Euismod quis viverra nibh
-            cras pulvinar mattis nunc. Arcu felis bibendum ut tristique et egestas
-            ipsum. Sed risus ultricies tristique nulla aliquet."
+    <S_card1 date={13} month="MAY" title="Dotted design summertime swimsuit collection for toddlers"
+        des="Pretium fusce id velit ut tortor. Euismod quis viverra nibh cras pulvinar mattis nunc. Arcu felis bibendum ut tristique et egestas ipsum. Sed risus ultricies tristique nulla aliquet. "
         more="READ MORE"
     />
 </div>
--- client/src/layouts/Secctions/section7.jsx
+++ client/src/layouts/Secctions/section7.jsx
@@ -1,14 +1,14 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "../../Styles/section2_style/sec7.css";
 
-import img1 from "../../assets/section7_img/product-category-01.png"
-import img2 from "../../assets/section7_img/Productt-Category-02.png"; 
-import img3 from "../../assets/section7_img/Product-Category-img-03.png"; 
-import img4 from "../../assets/section7_img/product-category-04.png"; 
-import img5 from "../../assets/section7_img/product-category-05.png"; 
-import img6 from "../../assets/section7_img/product-category-06.png"; 
-import img7 from "../../assets/section7_img/Product-Category-Img-07.png"; 
-import img8 from "../../assets/section7_img/product-category-08.png"; 
+import img1 from "../../assets/section7_img/product-category-01.png";
+import img2 from "../../assets/section7_img/Productt-Category-02.png";
+import img3 from "../../assets/section7_img/Product-Category-img-03.png";
+import img4 from "../../assets/section7_img/product-category-04.png";
+import img5 from "../../assets/section7_img/product-category-05.png";
+import img6 from "../../assets/section7_img/product-category-06.png";
+import img7 from "../../assets/section7_img/Product-Category-Img-07.png";
+import img8 from "../../assets/section7_img/product-category-08.png";
 
 const images = [
   { src: img1, label: "Toys" },
@@ -18,11 +18,11 @@ const images = [
   { src: img5, label: "New" },
   { src: img6, label: "Bottles" },
   { src: img7, label: "Cribs" },
-  { src: img8, label: "Beds" }
+  { src: img8, label: "Beds" },
 ];
 
 const Section7 = () => {
-  const [index, setIndex] = useState(0);
+  const [currentImages, setCurrentImages] = useState(images);
 
   // Auto-slide every 1 second
   useEffect(() => {
@@ -30,33 +30,44 @@ const Section7 = () => {
       nextSlide();
     }, 1000);
     return () => clearInterval(interval);
-  }, [index]);
+  }, [currentImages]);
 
   const nextSlide = () => {
-    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    // Move the last image to the first position
+    setCurrentImages((prevImages) => {
+      const lastImage = prevImages[prevImages.length - 1];
+      const newImages = [lastImage, ...prevImages.slice(0, prevImages.length - 1)];
+      return newImages;
+    });
   };
 
   const prevSlide = () => {
-    setIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    // Move the first image to the last position
+    setCurrentImages((prevImages) => {
+      const firstImage = prevImages[0];
+      const newImages = [...prevImages.slice(1), firstImage];
+      return newImages;
+    });
   };
 
   return (
     <div className="slider-container">
-      <button className="arrow left" onClick={prevSlide}>&lt;</button>
+      <button className="arrow left" onClick={prevSlide}>
+        &lt;
+      </button>
       <div className="slider">
-        {images.map((img, i) => {
-          let position = (i - index + images.length) % images.length;
-          return (
-            <div key={i} className={`slide ${position === 0 ? "active" : ""}`}>
-              <img src={img.src} alt={img.label} className="circle-image" />
-              <p>{img.label}</p>
-            </div>
-          );
-        })}
+        {currentImages.map((img, i) => (
+          <div key={i} className="slide">
+            <img src={img.src} alt={img.label} className="circle-image" />
+            <p>{img.label}</p>
+          </div>
+        ))}
       </div>
-      <button className="arrow right" onClick={nextSlide}>&gt;</button>
+      <button className="arrow right" onClick={nextSlide}>
+        &gt;
+      </button>
     </div>
   );
-}
+};
 
-export default Section7
+export default Section7;
